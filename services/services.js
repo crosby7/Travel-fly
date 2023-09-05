63c2f15ffede7f3cdd72bca27d2216178f6aea26
crosby7 <camcrosby09@gmail.com>
2023-09-05 12:09:46 -0400

    Full Project Republish

---
+++ services/services.js
@@ -0,0 +1,35 @@
+let globalNav = document.getElementById("globalNav");
+let navLogo = document.getElementById("navLogo");
+export var hamburgerMenu = document.getElementById("hamburgerMenu");
+
+export function loadPage() {
+    let hashTag = window.location.hash;
+    let pageToLoad = hashTag.replace("#", "");
+
+
+    if (pageToLoad != "")
+    {
+        $.get(`pages/${pageToLoad}.html`, function (data) {
+            $("#contentWrapper").html(data);
+
+            if (pageToLoad === "home")
+            {
+                document.body.classList.add("home");
+                navLogo.src = "images/logo/logo-white.png";
+            }
+            else
+            {
+                document.body.classList.remove("home");
+                navLogo.src = "images/logo/logo-black.png";
+            }
+        })
+    }
+    else
+    {
+        $.get(`pages/home.html`, function (data) {
+            $("#contentWrapper").html(data);
+            document.body.classList.add("home");
+            navLogo.src = "images/logo/logo-white.png";
+        })
+    }
+}
